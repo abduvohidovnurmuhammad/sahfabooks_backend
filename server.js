fef2cb1452aeb63d44d5f8b16533cdd6a3648fd4
abduvohidovnurmuhammad <abduvohidovn7@gmail.com>
2025-10-27 18:19:33 +0500

    cors qoyildi

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const cors = require('cors');
 require('dotenv').config();
 const db = require('./database');
 
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));  // ← BURAGA QO'SHING
 app.use('/uploads', express.static('uploads'));
